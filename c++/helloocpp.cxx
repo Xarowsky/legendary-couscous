5ff23c9f46da669172d2fe6839139e1793e87e06
xarowsky <wiktorch@vp.pl>
2017-09-20 12:36:07 +0200

    koniec lekcji

--- c++/helloocpp.cxx
+++ c++/helloocpp.cxx
@@ -4,12 +4,18 @@
 
 
 #include <iostream>
-
+//biblioteki
 using namespace std;
-
+//koniec bibliotek
 int main(int argc, char **argv)
 {
-	cout << "hello c++";
+    char imie[10]; //deklaracja zmiennej znakowej //[] deklaracja tablicy znaków
+    
+	cout << "hello c++" << endl; //cout print onscreen
+    cout << "Podaj imię:";
+    //cin >> imie;
+    cin.getline(imie, 10) >> imie; //funkcje po .
+    cout << "Cześć " << imie << endl;
 	return 0;
 }
-
+//zmiennna to nazwa której można przypisać dane lub je odczytać
