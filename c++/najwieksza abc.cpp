283aa28ea30b9002ff676c2c9fd9cb436fb32623
Xarowsky <31763254+Xarowsky@users.noreply.github.com>
2017-09-26 19:41:09 +0200

    praca domowa

--- c++/najwieksza abc.cpp
+++ c++/najwieksza abc.cpp
@@ -11,27 +11,33 @@ int main(int argc, char **argv) {
     a = 0;
     b = 0;
     c = 0;
-    cout << "podaj liczbę A: ";
+    cout << "podaj liczbe A: ";
     cin >> a;
-    cout << "podaj liczbę B: ";
+    cout << "podaj liczbe B: ";
     cin >> b;
-    cout << "podaj liczbę C: ";
+    cout << "podaj liczbe C: ";
     cin >> c;
-    
+
     if (a>b)
     {
         if (a>c)
-            cout << "największe A = " << a;
+            cout << "najwieksze A = " << a;
+                else
+                cout << "njwieksze C =" << c;
     }
     else if (a<b)
     {
         if (b>c)
-        cout << "największe B = " << b;
+        cout << "najwieksze B = " << b;
+            else
+            cout << "najwieksze C = " << c;
     }
     else if (c>a)
     {
         if (c>b)
-        cout << "największe C = " << c;
+        cout << "najwieksze C = " << c;
+            else
+            cout << "najwieksze B = " << c;
     }
     else //if (a==b)
     {
