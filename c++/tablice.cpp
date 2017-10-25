a06ffc7926cce94eb592cbb5fac71a50296f7968
xarowsky <wiktorch@vp.pl>
2017-10-25 12:34:45 +0200

    sync

--- c++/tablice.cpp
+++ c++/tablice.cpp
@@ -2,23 +2,35 @@
 
 using namespace std;
 
+int sumuj(int tab[])
+{
+    int suma=0;
+    int i=0
+    for(i=0;i<5;i++)
+    {
+        cout<<"podana liczba nr."<<i<<"="<<tab[i]<<endl;
+        suma=suma+tabela[i];
+        }
+    return suma;
+    }
+
+void pobierzdane(int tab[],int ile)
+{
+    int i;
+    cout <<"podaj " <<ile<<" liczb:"<<endl;
+    for(i=0;i<ile;i++) {cin>>tab[i];}
+    }
 int main(int argc, char **argv)
 {
 const int rozmiar=5;
-int srednia=0;
+float srednia=0;
 int suma = 0;
-int i = 0;
 int tabela[5];
-cout<<"podaj pięć liczb: ";
 
-for(i=0;i<5;i++)
-    {cin>>tabela[i];}
+pobierzdane(tabela,rozmiar);
     
-for(i=0;i<5;i++)
-    {
-        cout<<"podana liczba nr."<<i<<"="<<tabela[i]<<endl;
-        suma=suma+tabela[i];
-        }
+sumuj(tabela);
+
     srednia=float(suma)/float(5);
     cout<<"suma "<<suma<<endl;
     cout<<"srednia  "<<srednia;
