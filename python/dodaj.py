a8d82dc111c6726da9088a86e6ab71d8b246f6bd
xarowsky <wiktorch@vp.pl>
2017-10-31 13:31:12 +0100

    sync

---
+++ python/dodaj.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def sumuj(x,y):
+    return x+y
+def odejmij(x,y):
+    return x-y
+def iloraz(x,y):
+    
+a=int(input('podaj liczbę a:\n'))
+b=int(input('podaj liczbę b:\n'))
+print('suma: ',sumuj(a, b)
+print('roznica: ', odejmij(a,b))
---
+++ python/wstep.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#osoba = "Niet"
+#osoba = 'Niet'
+osoba = input("Jak się nazywasz?\n")
+print("Witaj ",osoba,"!")
+ 
+rok = 2017
+wiek = int(input('ile masz lat?\n'))
+print('urodziłeś się w ', rok-wiek , 'roku!')
