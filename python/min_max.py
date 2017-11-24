00af97269b621909435263b06ddcda10ee464cc5
xarowsky <wiktorch@vp.pl>
2017-11-24 12:43:42 +0100

    minmax2

--- python/min_max.py
+++ python/min_max.py
@@ -26,13 +26,35 @@ def minimum(lista):
     return min
 
 
+def min_max(lista):
+    lmin = [0]
+    lmax = [0]
+    for i, el in enumerate(lista):
+        if i > lista[i + 1]:
+            el = lmin
+            lista[i + 1]
+        else:
+            el = lmax
+            lista[i + 1] = lmin
+    max = lmax[0]
+    for i in lmax:
+        if i > max:
+            max = i
+    min = lmin[0]
+    for i in lmin:
+        if i < min:
+            min = i
+    return min, max
+
+
 def main(args):
     ile = 20
     zakres = 50
     lista = losuj(ile, zakres)
     print(lista)
-    print(minimum(lista))
-    print(maksimum(lista))
+    # print(minimum(lista))
+    # print(maksimum(lista))
+    print(min_max(lista))
 
 
 if __name__ == '__main__':
