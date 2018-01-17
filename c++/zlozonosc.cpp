e37fb07bd6afcd9e9354e8ee530d1ca2586ca243
Xarowsky <push.default>
2018-01-17 12:35:56 +0100

    notatki

---
+++ c++/zlozonosc.cpp
@@ -0,0 +1,20 @@
+/*
+ * zlozonosc.cxx
+ */
+
+
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+    int n = 0
+	cout<<"podaj liczbe n ";
+    cin>>n
+    for(int i = 1;i != n; i+=2) {
+    cout<< i << "0"
+    }
+	return 0;
+}
+
